ab949de3be11e4cccf3241eabfffa7893a70904f
Juhan Aasaru <aasaru@gmail.com>
2025-06-26 17:05:32 +0300

    add solution to first assignment

---
+++ solution-a01/src/main/java/dev/wisest/getstarted/BeansController.java
@@ -0,0 +1,54 @@
+package dev.wisest.getstarted;
+
+/*-
+ * #%L
+ * "Learn Spring Boot by Examining 10+ Practical Applications" course materials
+ * %%
+ * Copyright (C) 2025 Juhan Aasaru and Wisest.dev
+ * %%
+ * The source code (including test code) in this repository is licensed under a
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * %
+ * Attribution-NonCommercial-NoDerivatives 4.0 International License is a standard
+ * form license agreement that does not permit any commercial use or derivatives
+ * of the original work. Under this license: you may only distribute a verbatim
+ * copy of the work. If you remix, transform, or build upon the work in any way then
+ * you are not allowed to publish nor distribute modified material.
+ * You must give appropriate credit and provide a link to the license.
+ * %
+ * The full license terms are available from
+ * <http://creativecommons.org/licenses/by-nc-nd/4.0/legalcode>
+ * %
+ * All the code (including tests) contained herein should be attributed as:
+ * (Copyright) Juhan Aasaru https://Wisest.dev
+ * #L%
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+
+
+@Controller
+@ResponseBody
+public class BeansController {
+
+	private final ApplicationContext context;
+
+	public BeansController(ApplicationContext applicationContext) {
+		this.context = applicationContext;
+	}
+
+	@GetMapping("/beans")
+	public String[] listBeans() {
+		System.out.println("Let's return the beans provided by Spring Boot:");
+
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		return beanNames;
+	}
+
+}
