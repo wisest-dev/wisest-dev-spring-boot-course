cb3d8772697d559d6c04ad2de2cec7d042579fed
Juhan Aasaru <aasaru@gmail.com>
2025-10-11 17:28:47 +0300

    fix package

--- a02-serve-web-content/src/test/java/dev/wisest/servewebcontent/lecture/A02ErrorPageTest.java
+++ a02-serve-web-content/src/test/java/dev/wisest/servewebcontent/lecture/A02ErrorPageTest.java
@@ -29,7 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.test.client.TestRestTemplate;
 import org.springframework.http.*;
 
 import java.time.LocalDateTime;
